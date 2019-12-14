9741c15329b4705e504ebd63b4218f683c24467c
livia <liviasou07@gmail.com>
2019-12-14 20:20:50 -0300

    Descricao do commit

---
+++ distancia.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+// função que calcula a distância 
+float calculaDistancia(int x1, int y1, int x2, int y2){   
+    return sqrt( pow(x2-x1,2)+ pow(y2-y1, 2) );
+}
+// main
+int main() {
+
+
+
+
+
+
+    
+
+}
